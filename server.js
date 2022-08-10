e16b35d4fc2cf29df257a58a5f55eb4cac4deb6f
rahulnarayananuws <rahul.narayanan@yahoo.com>
2022-08-10 03:10:09 +0100

    hosting a json-server

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+// const jsonServer = require('json-server')
+// const server = jsonServer.create()
+// const router = jsonServer.router('db.json')
 
-const middlewares = jsonServer.defaults({
-    static:'./dist/gi-app'
-})
+// const middlewares = jsonServer.defaults({
+//     static:'./dist/gi-app'
+// })
 
-server.use(middlewares)
-server.use(router)
+// server.use(middlewares)
+// server.use(router)
 
-const port = process.env.PORT || 3000
+// const port = process.env.PORT || 3000
 
-server.listen(port, () => {
-    console.log(`JSON Server is running on port ${port}`)
-})
+// server.listen(port, () => {
+//     console.log(`JSON Server is running on port ${port}`)
+// })
+
+const jsonServer = require("json-server");
+const path = require("path");
+const express = require("express");
+const app = express();
+const router = jsonServer.router("db.json");
+const PORT = process.env.PORT || 3000;
+
+app.use("/", express.static("build"), router);
+
+if (process.env.NODE_ENV === "production") {
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname + "./build/index.html"));
+  });
+}
+
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
