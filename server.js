32ab4e896c90570215762b76d3ceeed2bf21ebbc
rahulnarayananuws <rahul.narayanan@yahoo.com>
2022-08-10 03:36:54 +0100

    added changes

--- server.js
+++ server.js
@@ -1,35 +1,10 @@
-// const jsonServer = require('json-server')
-// const server = jsonServer.create()
-// const router = jsonServer.router('db.json')
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-// const middlewares = jsonServer.defaults({
-//     static:'./dist/gi-app'
-// })
+server.use(middlewares);
+server.use(router);
 
-// server.use(middlewares)
-// server.use(router)
-
-// const port = process.env.PORT || 3000
-
-// server.listen(port, () => {
-//     console.log(`JSON Server is running on port ${port}`)
-// })
-///
-const jsonServer = require("json-server");
-const path = require("path");
-const express = require("express");
-const app = express();
-const router = jsonServer.router("db.json");
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(__dirname + "/dist/gi-app"))
-
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('dist/gi-app'));
-}
-
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'dist/gi-app', 'index.html'));
-});
-
-app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+server.listen(port);
